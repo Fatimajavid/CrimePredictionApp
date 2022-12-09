1785e138951cf805f9685de5474a183d80b4c195
JianHui (Jake) Li <LiJake2001@gmail.com>
2022-12-09 12:33:09 -0500

    rename file

--- pages/Visualizations.py
+++ pages/Visualizations.py
@@ -5,6 +5,6 @@ image = Image.open("actual_vs_pred.png")
 new_image = image.resize((600, 400))
 st.image(new_image)
 
-image = Image.open("state_frequencies2.png")
+image = Image.open("state_frequencies.png")
 new_image = image.resize((1000, 1000))
 st.image(image)

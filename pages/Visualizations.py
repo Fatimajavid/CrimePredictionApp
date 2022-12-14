7f4ca64e9a9b6c2e365ebd4f08c9f5f29661bc31
JianHui (Jake) Li <LiJake2001@gmail.com>
2022-12-14 10:59:21 -0500

    change directory

--- pages/Visualizations.py
+++ pages/Visualizations.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open("actual_vs_pred.png")
+image = Image.open("visuals/actual_vs_pred.png")
 new_image = image.resize((600, 400))
 st.image(new_image)
 
-image = Image.open("state_frequencies.png")
+image = Image.open("visuals/state_frequencies.png")
 new_image = image.resize((1000, 1000))
 st.image(image)
